236ac40c78d145e9f1eb2f53ac8ebda486b12681
psophielam <psophielam92@gmail.com>
2020-07-09 21:32:44 +0200

    iteration 2 ongoing drawing

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,27 +1,69 @@
 class HangmanCanvas {
   constructor(secretWord) {
     this.context = document.getElementById('hangman').getContext('2d');
-    // ... your code goes here
+    this.secretWord = secretWord;
   }
 
   createBoard() {
-    // ... your code goes here
+    this.context.clearRect(0,0, this.context.canvas.width, this.context.canvas.height);
+    this.drawLines();
   }
 
   drawLines() {
-    // ... your code goes here
+    /*
+    this.context.beginPath();
+    var splitted = hangman.secretWord.split('');
+    splitted.forEach((el, index)=> {
+      this.context.moveTo(200+index*50, 750);
+      this.context.lineTo(250+index*50, 750);
+    })
+  
+    this.context.closePath();
+    this.context.lineWidth = 5;
+    this.context.stroke();
+    */
+   this.context.beginPath()
+		hangman.secretWord.split('').forEach((letter, i) => {
+			this.context.moveTo(200 + i * 50, 775)
+			this.context.lineTo(240 + i * 50, 775)
+		})
+    this.context.closePath()		
+    this.context.lineWidth = 5;
+		this.context.stroke()
   }
 
   writeCorrectLetter(letter) {
-    // ... your code goes here
+
+    this.context.fillText(letter, 250, 300);
   }
 
   writeWrongLetter(letter, errorsLeft) {
     // ... your code goes here
   }
+// drawHangman(errorsLeft) {
+  drawHangman() {
+    this.context.beginPath();
+    this.context.moveTo(50,775);
+    this.context.lineTo(150, 775);
+    this.context.closePath();
+    this.context.lineWidth = 5;
+    this.context.stroke();
 
-  drawHangman(errorsLeft) {
-    // ... your code goes here
+    this.context.beginPath();
+    this.context.moveTo(50,775);
+    this.context.lineTo(100, 730);
+    this.context.closePath();
+    this.context.lineWidth = 5;
+    this.context.stroke();
+
+    this.context.beginPath();
+    this.context.moveTo(150,775);
+    this.context.lineTo(100, 730);
+    this.context.closePath();
+    this.context.lineWidth = 5;
+    this.context.stroke();
+
+
   }
 
   gameOver() {
