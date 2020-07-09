df2ff5341fd944963020d5ad50f8bdfc74ca32be
Enine Chareunsouk <enine.chareunsouk@gmail.com>
2020-07-09 21:48:46 +0200

    done until writeCorrectLetter

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,23 +1,44 @@
 class HangmanCanvas {
   constructor(secretWord) {
     this.context = document.getElementById('hangman').getContext('2d');
-    // ... your code goes here
+    this.createBoard();
+
   }
 
   createBoard() {
-    // ... your code goes here
+    this.context.clearRect(0, 0, 1200, 800);
+    this.drawLines(); //??
   }
 
   drawLines() {
-    // ... your code goes here
+    const longTrait = 32
+    let ecart = 17
+    let x = 205
+
+    for (let i = 0; i < hangman.secretWord.length; i++) {
+      this.context.beginPath();
+      this.context.moveTo(x, 778) //x = 254
+      this.context.lineTo(x + longTrait, 778); // trait = 286 / Position x = 286
+      // this.context.moveTo(254, 778)
+      this.context.closePath();
+      this.context.stroke();
+      x = x + ecart + longTrait // x = 303
+    }
   }
 
   writeCorrectLetter(letter) {
-    // ... your code goes here
+    this.context.font = '40px Arial' // UTILE POUR AFFICHER LA POLICE SINON CA NE MARCHERA PAS
+    hangman.secretWord.split('').forEach((element, index) => {
+      if (element === letter) {
+        this.context.fillText(element, 285 + index * 17, 770);
+      }
+
+
+    })
   }
 
   writeWrongLetter(letter, errorsLeft) {
-    // ... your code goes here
+
   }
 
   drawHangman(errorsLeft) {
