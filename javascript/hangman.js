43a2a1b0f6f75779d94bde45dc679f74754aa51d
Laura Bonavent <laura.bonavent@gmail.com>
2020-07-09 20:04:29 +0200

    iteration 1 modifiée

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -2,34 +2,59 @@ class Hangman {
   constructor(words) {
     this.words = words;
     // ... your code goes here
+    this.secretWord = this.pickWord();
+    this.letters = []; // all unique letters tried
+    this.guessedLetters = ''; // the already found letters
+    this.errorsLeft = 8;
   }
-
   pickWord() {
     // ... your code goes here
+    const rand = Math.floor(Math.random()*this.words.length);
+    return this.words[rand];
   }
-
-  checkIfLetter(keyCode) {
+  checkIfLetter(key) {
     // ... your code goes here
+    const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    if (alphabet.includes(key)) {
+      return true;
+    } else {
+      return false;
+    }
   }
-
   checkClickedLetters(letter) {
     // ... your code goes here
+    if (this.letters.includes(letter)) {
+      // already present
+      return false;
+    } else {
+      // not already present
+      this.letters.push(letter);
+      return true;
+    }
   }
-
   addCorrectLetter(letter) {
     // ... your code goes here
+    this.guessedLetters += letter;
   }
-
   addWrongLetter(letter) {
     // ... your code goes here
+    this.errorsLeft--;
   }
-
   checkGameOver() {
     // ... your code goes here
+    if (this.errorsLeft > 0) {
+      return false;
+    } else {
+      return true;
+    }
   }
-
   checkWinner() {
     // ... your code goes here
+    if (this.guessedLetters.length === this.secretWord.length) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
